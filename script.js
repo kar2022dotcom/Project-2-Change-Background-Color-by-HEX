28e7ab78f333d96e04c9377bcb6b72c8f0b9f810
kar2022dotcom <117594615+kar2022dotcom@users.noreply.github.com>
2022-12-18 22:50:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('#btn');
+
+
+
+btn.addEventListener('click', function(){
+    let hexParts = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let empty = '';
+
+    for(let i = 0; i < 6; i++){
+        let randomHex = Math.floor(Math.random() * hexParts.length);
+        empty += hexParts[randomHex];
+    }
+
+    document.body.style.backgroundColor = `#${empty}`;
+    btn.innerHTML = `${empty}`
+})
+
+// Cool feature to run all variation of HEX colors by setInterval
+/*
+setInterval(function(){
+    let hexParts = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let empty = '';
+
+    for(let i = 0; i < 6; i++){
+        let randomHex = Math.floor(Math.random() * hexParts.length);
+        empty += hexParts[randomHex];
+    }
+
+    document.body.style.backgroundColor = `#${empty}`;
+    btn.innerHTML = `${empty}`
+},1000)
+*/
